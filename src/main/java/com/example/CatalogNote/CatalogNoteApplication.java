5d5bec4d7aaf490127c7c8c35d8af6b95e2281f5
PGabrielC <125990452+PGabrielC@users.noreply.github.com>
2024-04-09 19:08:58 +0300

    Add files via upload
    spring initializr setup

---
+++ src/main/java/com/example/CatalogNote/CatalogNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatalogNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CatalogNote/CatalogNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatalogNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
