d86ff4f9f70c2b83c9773c793a64d2596c37a3e4
Marin Elena Catalina <126235888+marinelenacatalina@users.noreply.github.com>
2024-04-09 19:54:13 +0300

    react-roter-dom package installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        {/* <Route path='/' element={<ProtectedRoute><MainPage /></ProtectedRoute>} /> */}
+{/* 
+        <Route path='/login' element={<RedirectIfLoggedIn><LoginPage /></RedirectIfLoggedIn>} />
+     
+        <Route path="*" element={<Missing />} /> */}
+      </Routes>
     </div>
   );
 }
