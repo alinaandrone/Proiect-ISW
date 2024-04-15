7f9975efb680dd3037fdd4b40314ef5ce50ad69c
Marin Elena Catalina <126235888+marinelenacatalina@users.noreply.github.com>
2024-04-15 19:26:37 +0300

    create a new student/teacher as an admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
         {/* <Route path='/' element={<ProtectedRoute><MainPage /></ProtectedRoute>} /> */}
 {/* 
         <Route path='/login' element={<RedirectIfLoggedIn><LoginPage /></RedirectIfLoggedIn>} />
